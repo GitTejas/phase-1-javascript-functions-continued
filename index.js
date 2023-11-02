51680277e7503c0c217621892c908e20bf11e30f
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-02 14:52:19 -0400

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+       return `This Saturday, I want to ${activity}!`;
+    }
+saturdayFun()
+// code your solution here
+function mondayWork(monday= "go to the office") {
+    return `This Monday, I will ${monday}.`
+}
+
+// code your solution here
+function wrapAdjective(string="*") {
+    return function(word="special") {
+        return `You are ${string}${word}${string}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
